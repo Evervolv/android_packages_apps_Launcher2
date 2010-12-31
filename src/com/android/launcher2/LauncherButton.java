176dd6c6e3fd2a5e4f8a000617fd8cd24ffd2c88
preludedrew <preludedrew@gmail.com>
2010-12-31 00:11:29 -0700

    first commit

---
+++ src/com/android/launcher2/LauncherButton.java
@@ -0,0 +1,13 @@
+package com.android.launcher2;
+
+import android.content.Context;
+import android.widget.ImageView;
+
+public class LauncherButton extends ImageView{
+
+	public LauncherButton(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+
+}
